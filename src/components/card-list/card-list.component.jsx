6a4a35c22af10b84d00a1be5119a2e88d8760741
Joseph <you@example.com>
2021-10-27 02:11:28 -0700

    refactored

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import './card-list.styles.css';
-import { Card } from '../card/card.component';
+import React from "react";
+import "./card-list.styles.css";
+import { Card } from "../card/card.component";
 
 export const CardList = (props) => {
-    return(<div className='card-list'>
-             {props.monsters.map((monster) => (
-           <div>
-            <Card key={monster.id} monster = {monster}/>
-           </div>
-        ))}
-        </div>)
-}
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster) => (
+        <div>
+          <Card key={monster.id} monster={monster} />
+        </div>
+      ))}
+    </div>
+  );
+};
